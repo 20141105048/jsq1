38a21bc592292c17ed8126537b901cd0802c2cca
20141105048 <l20141105048@king.local>
2016-06-23 09:03:31 +0800

    Initial Commit

---
+++ jsq1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jsq1
+//
+//  Created by l20141105048 on 16/6/23.
+//  Copyright © 2016年 l20141105048. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
